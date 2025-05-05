81c4c357d6795c64330ec4d8d59cde96f27803cf
21IggyD <ignasadraugelis@gmail.com>
2025-05-05 19:47:19 -0400

    Update script.js

--- docs/jscript/script.js
+++ docs/jscript/script.js
@@ -2,26 +2,28 @@ window.onload = function () {
     const popup = document.getElementById('popup');
     const closeBtn = document.getElementById('closePopup');
     const nameInput = document.getElementById("name");
+    const form = document.getElementById("donationForm");
   
     popup.style.display = 'flex';
   
     closeBtn.onclick = function () {
       popup.style.display = 'none';
     };
-  };
-  form.onsubmit = function (event) {
-    event.preventDefault(); 
-
-    const name = nameInput.value.trim(); 
-
-    // If Then Else
-    if (name.toLowerCase() === "admin") {
-      alert("Hello Admin! Thank you for supporting us!");
-    } else if (name === "") {
-      alert("Please enter your name.");
-    } else {
-      alert(`Thank you, ${name}, for your donation!`);
-    }
-
-    popup.style.display = "none";
+  
+    form.onsubmit = function (event) {
+      event.preventDefault(); 
+  
+      const name = nameInput.value.trim(); 
+  
+      // If Then Else
+      if (name.toLowerCase() === "admin") {
+        alert("Hello Admin! Thank you for supporting us!");
+      } else if (name === "") {
+        alert("Please enter your name.");
+      } else {
+        alert(`Thank you, ${name}, for your donation!`);
+      }
+  
+      popup.style.display = "none";
+    };
   };
