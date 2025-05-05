e537790e24f2f23999ee4b23397d3cf8435684b9
21IggyD <ignasadraugelis@gmail.com>
2025-05-05 19:28:32 -0400

    update

---
+++ docs/jscript/script.js
@@ -0,0 +1,9 @@
+<script>
+  window.onload = function() {
+    document.getElementById('popup').style.display = 'flex'
+  };
+
+  document.getElementById('closePopup').onclick = function() {
+    document.getElementById('popup').style.display = 'none'
+  };
+</script>
